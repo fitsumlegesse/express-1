78760213c73e70a9ac694085b90459a3019f710f
Fitsum Legesse <55632280+fitsumlegesse@users.noreply.github.com>
2020-05-20 11:16:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 var  express = require('express');
+var data = require('./data/test.json');
+
+
+/*
+
+import data from './data/test.json';
+
+*/
+
+
+
 var app = express();
 const PORT = 3000;
 
@@ -13,6 +24,8 @@ app.use(express.static(__dirname));
 app.listen(PORT,()=>{
 
     console.log(`app is running on port ${PORT}`);
+    console.log(data);
+    
 
 
 
